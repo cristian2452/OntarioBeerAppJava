f850e75ecac1bbba68cfb23d83976a355c99a1c2
Jorge Atempa <atempaitm@gmail.com>
2019-03-17 14:40:02 -0700

    Fetch data from API Service

--- app/src/main/java/com/demo/ontariobeerapp/MainActivity.java
+++ app/src/main/java/com/demo/ontariobeerapp/MainActivity.java
@@ -2,12 +2,48 @@ package com.demo.ontariobeerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = "ONTARIOBEERAPI_DEMO";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://ontariobeerapi.ca/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService service = retrofit.create(ApiService.class);
+        Call<List<Beer>> apiResponse = service.fetchBeers();
+        apiResponse.enqueue(new Callback<List<Beer>>() {
+            @Override
+            public void onResponse(Call<List<Beer>> call, Response<List<Beer>> response) {
+                List<Beer> beers = new ArrayList<Beer>();
+                beers = response.body();
+                assert beers != null;
+                for (Beer beer: beers) {
+                    Log.d(TAG, beer.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Beer>> call, Throwable t) {
+
+            }
+        });
     }
 }
