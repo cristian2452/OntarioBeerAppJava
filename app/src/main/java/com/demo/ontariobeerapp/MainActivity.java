26240af57207dc60cff9dbd39f9e36b631636ffc
Jorge Atempa <atempaitm@gmail.com>
2019-03-23 17:31:55 -0700

    Add Progress Bar

--- app/src/main/java/com/demo/ontariobeerapp/MainActivity.java
+++ app/src/main/java/com/demo/ontariobeerapp/MainActivity.java
@@ -4,9 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -15,11 +19,15 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG = "ONTARIOBEERAPI_DEMO";
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        progressBar.setVisibility(View.VISIBLE);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://ontariobeerapi.ca/")
@@ -33,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<List<Beer>> call, Response<List<Beer>> response) {
                 final List<Beer> beers = response.body();
                 assert beers != null;
+                progressBar.setVisibility(View.GONE);
                 final BeerAdapter adapter = new BeerAdapter(beers);
                 final RecyclerView recyclerView = findViewById(R.id.recycler_view);
                 final LinearLayoutManager linearLayout = new LinearLayoutManager(getBaseContext());
